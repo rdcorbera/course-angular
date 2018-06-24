a5beca7a5559c6ee12a6029aeb70e7cfcf0a0d83
Skynet <skynet@skynet-mbpro.local>
2018-06-24 16:41:32 -0500

    Add new guard

--- 04-a-bit-of-harmony/places-web/src/app/place/place-routing.module.ts
+++ 04-a-bit-of-harmony/places-web/src/app/place/place-routing.module.ts
@@ -4,9 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ResultComponent } from './components/result/result.component';
 import { DetailComponent } from './components/detail/detail.component';
+import { NewComponent } from './components/new/new.component';
+import { AdminOnlyGuard } from '../security/guards/admin-only.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'new', component: NewComponent, canActivate: [AdminOnlyGuard] },
   { path: 'search/:query', component: ResultComponent },
   { path: 'detail/:id', component: DetailComponent }
 ];
