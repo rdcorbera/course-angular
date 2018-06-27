2fd1584a0a701559824c2d55bc0e17f99daf1d71
Skynet <skynet@skynet-mbpro.local>
2018-06-26 21:12:59 -0500

    Add unit tests for user service

--- 05-dont-break-the-code/places-web/src/app/security/services/user.service.spec.ts
+++ 05-dont-break-the-code/places-web/src/app/security/services/user.service.spec.ts
@@ -1,15 +1,41 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { UserService } from './user.service';
+import { ProfileService } from './profile.service';
+import { User } from '../models/user';
+import { Profile } from '../models/profile';
+import { environment } from '../../../environments/environment.prod';
 
 describe('UserService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserService]
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [ProfileService, UserService]
     });
   });
 
   it('should be created', inject([UserService], (service: UserService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should return expected profile', inject(
+    [UserService, HttpTestingController], (service: UserService, backend: HttpTestingController) => {
+
+    const expectedProfile = new Profile(1, 'admin', 'Admin', 'Admin', 'admin@domain.com', new Date(), null, null, null);
+
+    service.login(new User('admin', 'admin', false)).subscribe(
+      profile => expect(profile.rol).toEqual('admin'),
+      fail
+    );
+
+    const req = backend.expectOne({
+      url: `${environment.apiUrl}user/login`,
+      method: 'POST'
+    });
+
+    req.flush(expectedProfile);
   }));
 });
